556dbdfa7911ee10ea59e430ad89f8eddcdf9c56
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:26:39 -0600

    sadhf

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -5,7 +5,7 @@ import random
 def setup(bot):
     async def message_events(message):
         if 'zeb' in message.clean_content.lower():
-            await bot.say(message.channel, 'I :heart: Zeb-kun')
+            await bot.send_message(message.channel, 'I :heart: Zeb-kun')
 
     bot.add_listener(message_events, 'on_message')
 
