f99546bd184aac12e4b7bd4007c718c45d2a9a04
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:21:05 -0600

    dsaflkjh

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -2,10 +2,9 @@ import discord
 from discord.ext import commands
 import random
 
-async def message_events(message):
-    await bot.say('its working')
-
-
 def setup(bot):
+    async def message_events(message):
+        await bot.say('its working')
+
     bot.add_listener(message_events, 'on_message')
 
