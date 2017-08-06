b766aee70920cffdf65ba2f40ecbfc9771a84769
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:08:21 -0600

    shenanigans

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -2,18 +2,9 @@ import discord
 from discord.ext import commands
 import random
 
-class Events:
-    def __init__(self, bot):
-        self.bot = bot
-
-        @bot.event
-        async def on_message(message):
-            """Parse and play with message"""
-            await bot.process_commands(message)
-            await self.bot.say(ctx.message)
-
+async def on_message(message):
+    print(message)
 
 def setup(bot):
-    n = Events(bot)
-    bot.add_cog(n)
+    bot.add_listener(on_message, 'on_message')
 
