7210e4cc83eb9b5071c8db536c383c31d1b3084e
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:18:14 -0600

    had to install ima doofus

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -3,13 +3,9 @@ from discord.ext import commands
 import random
 
 async def message_events(message):
-    print('personal func')
-    print(message)
+    await bot.say('its working')
+
 
 def setup(bot):
-    print('starting setup')
     bot.add_listener(message_events, 'on_message')
-    print('setup')
-
-    print(bot.extra_events)
 
