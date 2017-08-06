2556288e06f337efa431962262e53f5cb07691c6
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:16:05 -0600

    adding thins

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -2,9 +2,14 @@ import discord
 from discord.ext import commands
 import random
 
-async def on_message(message):
+async def message_events(message):
+    print('personal func')
     print(message)
 
 def setup(bot):
-    bot.add_listener(on_message)
+    print('starting setup')
+    bot.add_listener(message_events, 'on_message')
+    print('setup')
+
+    print(bot.extra_events)
 
