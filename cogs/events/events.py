89866a900604509fda3c6d38210841291b724359
William Farmer <willzfarmer@gmail.com>
2017-08-06 12:29:51 -0600

    bigfux

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -4,6 +4,9 @@ import random
 
 def setup(bot):
     async def message_events(message):
+        # DO NOT RESPOND TO SELF MESSAGES
+        if bot.user.id == message.author.id:
+            return
         if 'zeb' in message.clean_content.lower():
             await bot.send_message(message.channel, 'I :heart: Zeb-kun')
 
