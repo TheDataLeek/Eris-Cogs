f3e3ae4bdb78ca899008da0f7ea99682ced3edd5
William Farmer <willzfarmer@gmail.com>
2017-08-07 11:15:31 -0600

    too many commits

--- cogs/emoji/emoji.py
+++ cogs/emoji/emoji.py
@@ -1,13 +1,14 @@
 import discord
 from discord.ext import commands
 from .utils.dataIO import fileIO
+from typing import Union
 
 class Emoji:
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True, no_pm=True)
-    async def emoji(self, ctx, emoji: discord.Emoji):
+    async def emoji(self, ctx, emoji: Union[str, discord.Emoji]):
         """ Expand an emoji """
         print(emoji)
         print(emoji.url)
