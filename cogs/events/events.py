d06e1a5c4d571c782b2d2a53a34a610fe5fa8de7
William Farmer <willzfarmer@gmail.com>
2017-08-07 14:34:26 -0600

    too many commits

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -37,10 +37,6 @@ dragonart = """
 
 
 def setup(bot):
-    # have to setup data first
-    with open('./data/penis.gif', 'r') as fobj:
-        penisgif = fobj
-
     async def message_events(message):
         # DO NOT RESPOND TO SELF MESSAGES
         if bot.user.id == message.author.id or message.content.startswith('.'):
@@ -67,7 +63,8 @@ def setup(bot):
         elif 'dragon' in message.clean_content.lower():
             await bot.send_message(message.channel, dragonart)
         elif 'penis' in message.clean_content.lower():
-            await bot.send_file(message.channel, penisgif, filename='penis.gif')
+            with open('./data/events/penis.gif', 'r') as fobj:
+                await bot.send_file(message.channel, fobj, filename='penis.gif')
 
     bot.add_listener(message_events, 'on_message')
 
