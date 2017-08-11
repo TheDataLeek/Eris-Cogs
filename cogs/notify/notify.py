4c663e9542aea540949a774cf727897a64f6df6f
William Farmer <willzfarmer@gmail.com>
2017-08-11 15:13:33 -0600

    too many commits

--- cogs/notify/notify.py
+++ cogs/notify/notify.py
@@ -7,6 +7,8 @@ from functools import reduce
 
 def setup(bot):
     async def message_events(message):
+        print(message.content)
+        print(message.clean_content)
         if 'masters' in [x.name.lower() for x in message.author.roles] and '@everyone' in message.clean_content:
             await bot.send_message(message.channel, 'test')
 
