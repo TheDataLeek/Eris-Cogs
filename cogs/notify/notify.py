26878bfef45535ce20a00d78b5bebe44dc179ece
William Farmer <willzfarmer@gmail.com>
2017-08-11 15:36:58 -0600

    too many commits

--- cogs/notify/notify.py
+++ cogs/notify/notify.py
@@ -20,14 +20,14 @@ class Notify:
                                    if len(x) > 0]
 
             if number in current_numbers:
-                await bot.say('This number is already registered')
+                await self.bot.say('This number is already registered')
                 return
 
             with open('./data/notify/numbers.txt', 'a') as fobj:
                 fobj.write(number)
                 fobj.write('\n')
         else:
-            await bot.say('Please provide a valid phone number')
+            await self.bot.say('Please provide a valid phone number')
 
     @commands.command(pass_context=True)
     async def deletenumber(self, ctx, number : str):
@@ -39,16 +39,16 @@ class Notify:
                                    if len(x) > 0]
 
             if number not in current_numbers:
-                await bot.say('This number is not registered')
+                await self.bot.say('This number is not registered')
                 return
 
             with open('./data/notify/numbers.txt', 'w') as fobj:
                 for num in current_numbers:
                     fobj.write(num)
                     fobj.write('\n')
-                await bot.say('{} has been deleted'.format(number))
+                await self.bot.say('{} has been deleted'.format(number))
         else:
-            await bot.say('Please provide a valid phone number')
+            await self.bot.say('Please provide a valid phone number')
 
     @commands.command(pass_context=True)
     async def listnumbers(self, ctx):
@@ -56,7 +56,7 @@ class Notify:
             current_numbers = [x for x in
                                fobj.read().split('\n')
                                if len(x) > 0]
-            await bot.say('The following numbers have been registered: {}'.format(', '.join(current_numbers)))
+            await self.bot.say('The following numbers have been registered: {}'.format(', '.join(current_numbers)))
 
 
 def setup(bot):
