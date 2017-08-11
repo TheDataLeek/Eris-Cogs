4944670a58451db9e600635e0f63d18dc0cf9c03
William Farmer <willzfarmer@gmail.com>
2017-08-11 15:11:59 -0600

    too many commits

---
+++ cogs/notify/notify.py
@@ -0,0 +1,14 @@
+import os
+import discord
+from discord.ext import commands
+import random
+from functools import reduce
+
+
+def setup(bot):
+    async def message_events(message):
+        if 'masters' in [x.name.lower() for x in message.author.roles] and '@everyone' in message.clean_content:
+            await bot.send_message(message.channel, 'test')
+
+    bot.add_listener(message_events, 'on_message')
+
