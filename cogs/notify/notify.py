885bb80866d765f72f9ef3e21077aee3ce7fcc2b
William Farmer <willzfarmer@gmail.com>
2017-08-11 16:18:02 -0600

    too many commits

--- cogs/notify/notify.py
+++ cogs/notify/notify.py
@@ -13,9 +13,7 @@ class Notify:
         self.client = Client()
 
         async def message_events(message):
-            print(list(message.clean_content))
-            print('@\u200bhere' in message.clean_content)
-            if '@here' in message.clean_content and 'masters' in [x.name.lower() for x in message.author.roles]:
+            if '@\u200bhere' in message.clean_content and 'masters' in [x.name.lower() for x in message.author.roles]:
                 with open('./data/notify/numbers.txt', 'r') as fobj:
                     current_numbers = [x for x in
                                        fobj.read().split('\n')
