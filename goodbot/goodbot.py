2cf9b61fcff75918734ed9aa3eacea3bf91775da
Zoë F <zoe@dataleek.io>
2019-03-29 17:16:05 -0600

    fopo

--- goodbot/goodbot.py
+++ goodbot/goodbot.py
@@ -112,6 +112,7 @@ class GoodBot(BaseCog):
         for userid, good, bad in db_results:
             try:
                 print(userid)
+                print(ctx.guild.get_member(userid))
                 results.append((ctx.guild.get_member(userid).nick, good - bad))
             except Exception as e:
                 print(e)
