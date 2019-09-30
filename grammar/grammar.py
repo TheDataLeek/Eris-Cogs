97f72a654cd8df4a0c2090e4e3242f1165f83615
Zoë Farmer <zoe@dataleek.io>
2019-09-30 14:02:41 -0600

    sadf

---
+++ grammar/grammar.py
@@ -0,0 +1,72 @@
+import os
+import time
+import re
+import discord
+from redbot.core import commands
+import random
+from functools import reduce
+import io
+
+import sqlite3
+import pathlib
+import csv
+
+import language_check as lc
+
+
+tool = lc.LanguageTool('en-US')
+
+BaseCog = getattr(commands, "Cog", object)
+
+
+class Grammar(BaseCog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        async def grammar_module(message):
+            if message.guild is None or int(message.guild.id) != 142435106257240064:
+                return
+            clean_message = message.clean_content.lower()
+            # MM: Added so list instead of string
+            message_split = clean_message.split(' ')
+
+            # BLACKLIST CHANNELS
+            blacklist = [
+                'news',
+                'rpg',
+                'events',
+                'recommends',
+                'politisophy',
+                'eyebleach',
+                'weeb-lyfe',
+                'out-of-context',
+                'jokes',
+                'anime-club',
+            ]
+
+            message_channel = message.channel.name.lower()
+
+            if (message_channel.name != 'bot-chat') or (
+                # DO NOT RESPOND TO SELF MESSAGES
+                (bot.user.id == message.author.id or message.content.startswith('.')) or
+                (message.channel.name is None) or
+                (reduce(
+                    lambda acc, n: acc or (n == message_channel),
+                    blacklist,
+                    False)) or
+                ('@' in clean_message) or
+                ('thank' in clean_message) or
+                ('http' in clean_message)
+                ):
+                return
+
+            ctx = await bot.get_context(message)
+
+            matches = tool.check(clean_message)
+
+            await ctx.send(lc.correct(clean_message, matches))
+
+            return
+
+        self.bot.add_listener(grammar_module, 'on_message')
+
