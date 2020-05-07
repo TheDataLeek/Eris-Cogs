a0c334a6a102240f5ef11b2dfaa399ee1bc50f6c
Zoë F <zoe@dataleek.io>
2020-05-07 09:45:31 -0600

    ooc

--- out_of_context/outofcontext.py
+++ out_of_context/outofcontext.py
@@ -20,6 +20,8 @@ class OutOfContext(BaseCog):
     def __init__(self, bot):
         self.bot = bot
 
+        self.message_log = {}
+
         self.quote_hash = dict()
         for quote in quotes:
             quote_words = [_ for _ in quote.lower().split() if len(_) > 3]
@@ -77,23 +79,51 @@ class OutOfContext(BaseCog):
             ):
                 return
 
+            # channel-specific logs for last 5 messages
+            chan_id = message.channel.id
+            if chan_id not in self.message_log:
+                self.message_log[chan_id] = [clean_message]
+            else:
+                self.message_log[chan_id].append(clean_message)
+                if len(self.message_log[chan_id] > 5):
+                    self.message_log[chan_id].pop(0)
+
             ctx = await bot.get_context(message)
-
-            split_message = clean_message.split(" ")
-
-            random.shuffle(split_message)
 
             if random.random() <= 0.99:  # 1% chance of activation
                 return
 
-            reply = random.choice(quotes)
-            for word in split_message:
-                if word in self.quote_hash:
-                    reply = random.choice(self.quote_hash[word])
-                    break
+            reply = self.get_quote(chan_id)
 
             async with ctx.typing():
                 sleep(1)
                 await message.channel.send(reply)
 
         self.bot.add_listener(out_of_context_handler, "on_message")
+
+    @commands.command()
+    async def what_do_you_think(self, ctx):
+        reply = self.get_quote(ctx.channel.id, most_recent=False)
+        async with ctx.typing():
+            sleep(1)
+            await ctx.send(reply)
+
+    def get_quote(self, channel_id, most_recent=True):
+        reply = random.choice(quotes)
+        if channel_id not in self.message_log:
+            return reply   # just random if no logs
+
+        split_msgs = [s.split(' ') for s in self.message_log[channel_id]]
+        if most_recent:
+            split_message = split_msgs[-1]   # just grab the last
+        else:
+            split_message = reduce(lambda a, b: a + b, split_msgs)
+        random.shuffle(split_message)
+        split_message = [s for s in split_message if len(s) > 3]
+
+        for word in split_message:
+            if word in self.quote_hash:
+                reply = random.choice(self.quote_hash[word])
+                break
+
+        return reply
