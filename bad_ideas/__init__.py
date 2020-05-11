9ad9ffa3e6d32198ebea815d3ef369948ad47870
Zoë F <zoe@dataleek.io>
2020-05-11 14:45:26 -0600

    Black

--- bad_ideas/__init__.py
+++ bad_ideas/__init__.py
@@ -1,6 +1,7 @@
-from .bad_ideas import BigName, Clone
+from .bad_ideas import BigName, Clone, Weave
 
 
 def setup(bot):
     bot.add_cog(BigName(bot))
     bot.add_cog(Clone(bot))
+    bot.add_cog(Weave(bot))
