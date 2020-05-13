c50ba37f16ca88eb49adb8b75b29ef1fd896dc35
Zoë F <zoe@dataleek.io>
2020-05-13 14:47:56 -0600

    Updated README

--- README.md
+++ README.md
@@ -3,3 +3,72 @@
 Welcome to my cogs, currently these are the only finished v3 cogs:
 
 * insult
+
+
+## Alot
+
+Listens to `on_message` and posts an alot whenever someone says "alot of things" instead of "a lot of things".
+
+## Bad Ideas
+
+Lets users rename any other user (except server owner), weave emoji, and clones the bot to look like a user.
+
+## Battle
+
+An rpg-inspired battle system where users can fight and gain XP from talking
+
+## Boo
+
+Add fun prefixes to nicknames
+
+## Dice
+
+Better dice roller
+
+## Event Config
+
+*(IN PROGRESS)* Global `on_message` configuration for these cogs
+
+## Events
+
+Bunch of fun events to mess with people while talking
+
+## Facts
+
+Bear or Snake facts
+
+## Goodbot
+
+Keeps track of user scores based on thumbs up and thumbs down
+
+## Grammar
+
+Spellchecker
+
+## Insult
+
+Insults users
+
+## Notify
+
+Uses twilio API to text out @everyone notifications for those that don't have discord but want to keep up with friend group.
+
+## Out of Context
+
+Randomly interjects a topical quote.
+
+## Sarcasm
+
+Mocks users randomly
+
+## Say
+
+Secret admin command to post a message to General
+
+## Whois
+
+Keeps track of real names for people who don't pay as close attention
+
+## Zalgo 
+
+Zalgo's or Uwu's text
---
+++ event_config /event_config.py
@@ -0,0 +1,130 @@
+import io
+import discord
+from redbot.core import commands, data_manager, Config, checks, bot
+from typing import Union
+
+
+BaseCog = getattr(commands, "Cog", object)
+
+
+class EventConfig(BaseCog):
+    def __init__(self, bot_instance):
+        self.bot = bot_instance
+
+        self.config = Config.get_conf(
+            self,
+            identifier=22222939019,
+            force_registration=True,
+            cog_name="event_config",
+        )
+
+        default_global = {
+            "guild_list": [],
+        }
+        default_guild = {
+            "channel_whitelist": ["general", "bot-chat"],
+            "channel_blacklist": ["announcements", "news"],
+        }
+        self.config.register_global(**default_global)
+        self.config.register_guild(**default_guild)
+
+    @commands.group()
+    async def econf(self, ctx):
+        pass
+
+    @econf.command()
+    @checks.is_owner()
+    async def show(self, ctx):
+        async with self.config.guild_list() as guild_list, self.config.guild(
+            ctx.guild
+        ).channel_whitelist() as whitelist, self.config.guild(
+            ctx.guild
+        ).channel_blacklist() as blacklist:
+            if len(guild_list) == 0:
+                await ctx.send("No servers have been registered!")
+            else:
+                await ctx.send("Servers: " + ", ".join(guild_list))
+
+            if len(whitelist) == 0:
+                await ctx.send("Whitelist empty!")
+            else:
+                await ctx.send("Whitelist: " + ", ".join(whitelist))
+
+            if len(blacklist) == 0:
+                await ctx.send("Blacklist empty!")
+            else:
+                await ctx.send("Blacklist: " + ", ".join(blacklist))
+
+    @econf.command()
+    @checks.is_owner()
+    async def add_server(self, ctx, *, server_name: str = ""):
+        if server_name == "" and ctx.guild is not None:
+            server_name = ctx.guild.name.lower()
+        elif server_name == "" and ctx.guild is None:
+            await ctx.send("Please provide a valid server name")
+            return
+
+        async with self.config.guild_list() as guild_list:
+            guild_list.append(server_name)
+            await ctx.send("Done")
+
+    @econf.command()
+    @checks.is_owner()
+    async def remove_server(self, ctx, *, server_name: str = ""):
+        if server_name == "" and ctx.guild is not None:
+            server_name = ctx.guild.name.lower()
+        elif server_name == "" and ctx.guild is None:
+            await ctx.send("Please provide a valid server name")
+            return
+
+        async with self.config.guild_list() as guild_list:
+            try:
+                guild_list.remove(server_name)
+                await ctx.send("Done")
+            except ValueError:
+                await ctx.send("Server was not in list!")
+
+    @econf.command()
+    @checks.mod()
+    async def whitelist(self, ctx, channel: Union[str, discord.TextChannel]=None):
+        self.black_or_white_list(ctx, 'whitelist', channel)
+
+    @econf.command()
+    @checks.mod()
+    async def blacklist(self, ctx, channel=None):
+        if channel is None:
+            channel = ctx.channel.name.lower()
+        elif not isinstance(channel, str):
+            if isinstance(channel, discord.TextChannel):
+                channel = channel.name.lower()
+            else:
+                await ctx.send("Please provide a channel!")
+                return
+        else:
+            channel = channel.lower()
+
+        async with self.config.guild(ctx.guild).channel_blacklist() as blacklist:
+            blacklist.append(channel)
+
+    async def black_or_white_list(self, ctx, which, channel):
+        if channel is None:
+            channel = ctx.channel.name.lower()
+        elif not isinstance(channel, str):
+            if isinstance(channel, discord.TextChannel):
+                channel = channel.name.lower()
+            else:
+                await ctx.send("Please provide a channel!")
+                return
+        else:
+            channel = channel.lower()
+
+        async with self.config.guild(ctx.guild).channel_whitelist() as whitelist:
+            whitelist.append(channel)
+
+        async with self.config.guild(ctx.guild).channel_blacklist() as blacklist:
+            try:
+                blacklist.remove(channel)
+            except ValueError:
+                pass
+
+        await ctx.send("Done")
