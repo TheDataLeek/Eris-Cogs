23a24910a31c94ec6116e7c53b0a07a12d206deb
maddie <maddie.groeling@gmail.com>
2020-05-21 16:01:07 -0600

    Add error messages to big_text

--- big_text/big_text.py
+++ big_text/big_text.py
@@ -12,7 +12,7 @@ class BigText(BaseCog):
     async def big_text(self, ctx):
         raw_msg = " ".join(ctx.message.clean_content.split(" ")[1:])
         if raw_msg is "":
-            return
+            await ctx.send("Message cannot be empty!")
 
         match_check = re.match("[A-Za-z ]", raw_msg.lower())
         if bool(match_check):
@@ -27,3 +27,5 @@ class BigText(BaseCog):
                     big_msg += " "
 
             await ctx.send(big_msg)
+        else:
+            await ctx.send("Message can only have A-Z characters and spaces")
