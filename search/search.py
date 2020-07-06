ca6ee872a3cf590f111e5914909bc7abd8f494d0
Zoë F <zoe@dataleek.io>
2020-07-06 09:39:33 -0600

    Fixed bug

--- search/search.py
+++ search/search.py
@@ -11,8 +11,8 @@ class Search(BaseCog):
         self.search_link = "https://en.wikipedia.org/w/api.php?action=opensearch&search={}&limit=1&namespace=0&format=json"
 
     @commands.command()
-    async def wiki(self, ctx, *term):
-        new_term = parse.quote(term)
+    async def wiki(self, ctx, *term:str):
+        new_term = parse.quote_plus(" ".join(term))
         search = self.search_link.format(new_term)
 
         async with aiohttp.ClientSession() as session:
