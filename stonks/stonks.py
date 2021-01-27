9454d0abd15cc9974ecf89e5f51b0cd653b5c7ea
Zoe Farmer <zoe@dataleek.io>
2021-01-27 11:10:45 -0700

    stonks

--- stonks/stonks.py
+++ stonks/stonks.py
@@ -2,13 +2,14 @@
 import random
 import re
 from pprint import pprint as pp
+from io import BytesIO
 
 # third party
 import discord
 from redbot.core import commands, bot
 
 import yfinance as yf
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -22,17 +23,36 @@ class Stonks(BaseCog):
 
     @commands.command()
     async def stock(self, ctx, ticker: str):
-        s = yf.Ticker(ticker).info
+        s = yf.Ticker(ticker)
+        history = s.history()
+        print(history.columns)
+        s = s.info
+
+        buf = BytesIO()
+
+        fig = plt.figure()
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        history[['Open']].plot(ax=ax)
+        plt.savefig(buf, format='png')
+        buf.seek(0)
 
         embed = discord.Embed(
             title=f"{s['longName']} ({ticker})",
             type='rich',
-            description=f"{s['open']}"
+            description=f"Opened at {s['open']}"
         )
-        await ctx.send(embed=embed)
-
+        img = discord.File(buf, filename=f"{ticker}.png")
+        await ctx.send(embed=embed, file=img)
 
 if __name__ == '__main__':
-    pp(yf.Ticker('GME').info)
+    # pp(yf.Ticker('GME').info)
+
+    # history = yf.Ticker('GME').history()
+    #
+    # fig = plt.figure()
+    # ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    # history[['Open']].plot(ax=ax)
+    #
+    # plt.show()
     # print(yf.Ticker('GME').history())
 
