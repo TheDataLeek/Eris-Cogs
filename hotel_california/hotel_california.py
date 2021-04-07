56d3015a6d96bcc80e4b551fd735b06596fac3f1
Zoë F <zoe@dataleek.io>
2021-04-07 10:12:46 -0600

    Punish for mods

--- hotel_california/hotel_california.py
+++ hotel_california/hotel_california.py
@@ -6,6 +6,8 @@ import discord
 from discord import utils
 from redbot.core import commands, data_manager, Config, checks
 
+from typing import List
+
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -15,11 +17,15 @@ class HotelCalifornia(BaseCog):
         self.bot: commands.Cog = bot
 
         self.signifier = "ಠ"
+        self.mod_signifier = 'bad mod'
 
-    async def find_role(self, ctx: commands.Context):
+    async def find_role(self, ctx: commands.Context, ismod=False):
         role: discord.Role = None
         for guild_role in ctx.guild.roles:
-            if self.signifier in guild_role.name.lower():
+            if ismod and self.mod_signifier in guild_role.name.lower():
+                role = guild_role
+                break
+            elif self.signifier in guild_role.name.lower():
                 role = guild_role
                 break
         else:
@@ -30,9 +36,20 @@ class HotelCalifornia(BaseCog):
     @commands.command(pass_context=True)
     @checks.mod()
     async def punish(self, ctx: commands.Context, user: discord.Member):
-        role = await self.find_role(ctx)
+        userroles: List[discord.Role] = user.roles
+        ismod: bool = any(r.permissions.administrator for r in userroles)
+        role = await self.find_role(ctx, ismod=ismod)
         if not user.bot:
             await user.add_roles(role)
+
+    @commands.command(pass_context=True)
+    @checks.mod()
+    async def free(self, ctx: commands.Context, user: discord.Member):
+        userroles: List[discord.Role] = user.roles
+        ismod: bool = any(r.permissions.administrator for r in userroles)
+        role = await self.find_role(ctx, ismod=ismod)
+        if not user.bot:
+            await user.remove_roles(role)
 
     @commands.command(pass_context=True)
     async def hotel_california(self, ctx: commands.Context):
@@ -43,15 +60,3 @@ class HotelCalifornia(BaseCog):
         user: discord.Member = msg.author
 
         await user.add_roles(role)
-
-    # @commands.command(pass_context=True)
-    # @checks.is_owner()
-    # async def free_your_mistress(self, ctx: commands.Context):
-    #     role = await self.find_role(ctx)
-    #     msg: discord.Message = ctx.message
-    #     user: discord.Member = msg.author
-
-    #     if str(user.id) != '142431859148718080':
-    #         return
-
-    #     await user.remove_roles(role)
