a0139e46b3ff33463f1b5d1131f9369955c9c87a
Zoe Farmer <zoe@dataleek.io>
2021-04-20 22:17:02 -0600

    pls work

--- wiggler/wiggly.py
+++ wiggler/wiggly.py
@@ -69,10 +69,9 @@ class Wiggle(BaseCog, ErisEventMixin):
         async with self.lock_config.channel(
             message.channel
         ).get_lock(), self.config.guild(ctx.guild).wiggle() as wigglelist:
-            allowed: bool = await self.allowed(ctx, message)
+            # allowed: bool = await self.allowed(ctx, message)
             # allowed &= random.random() <= 0.05
-            allowed &= random.random() <= 1
-            allowed &= authorid in wigglelist
+            allowed = authorid in wigglelist
 
             emoji = random.choice([self.emojis[e] for e in wigglelist[authorid]])
             await message.add_reaction(emoji)
