d7c00c171806e20a2e20f9b75060adec7197a791
Zoë F <zoe@dataleek.io>
2021-04-21 10:18:51 -0600

    Wiki ready

--- search/search.py
+++ search/search.py
@@ -12,13 +12,18 @@ class Search(BaseCog):
 
     @commands.command()
     async def wiki(self, ctx, *term: str):
+        """
+        Search wikipedia for provided search term, and return the first result.
+        """
         new_term = parse.quote_plus(" ".join(term))
         search = self.search_link.format(new_term)
 
         async with aiohttp.ClientSession() as session:
             async with session.get(search) as resp:
                 data = await resp.json()
-                if len(data) == 0:
+                try:
+                    contents = data[-1][0]
+                except IndexError:
                     await ctx.send("No pages found!")
                     return
-                await ctx.send(data[-1][0])
+                await ctx.send(contents)
