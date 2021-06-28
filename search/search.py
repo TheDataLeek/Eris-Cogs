bba53ae0e82e94a783652d9fc99976434c68ec08
Zoe Farmer <zoe@dataleek.io>
2021-06-27 18:41:26 -0600

    wolfram

--- search/search.py
+++ search/search.py
@@ -1,6 +1,10 @@
-from redbot.core import commands
+import os
 from urllib import parse
+from pprint import pprint as pp
+
+from redbot.core import commands
 import aiohttp
+import wolframalpha
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -8,7 +12,7 @@ BaseCog = getattr(commands, "Cog", object)
 class Search(BaseCog):
     def __init__(self, bot):
         self.bot = bot
-        self.search_link = "https://en.wikipedia.org/w/api.php?action=opensearch&search={}&limit=1&namespace=0&format=json"
+        self.wiki_search_link = "https://en.wikipedia.org/w/api.php?action=opensearch&search={}&limit=1&namespace=0&format=json"
 
     @commands.command()
     async def wiki(self, ctx, *term: str):
@@ -16,7 +20,7 @@ class Search(BaseCog):
         Search wikipedia for provided search term, and return the first result.
         """
         new_term = parse.quote_plus(" ".join(term))
-        search = self.search_link.format(new_term)
+        search = self.wiki_search_link.format(new_term)
 
         async with aiohttp.ClientSession() as session:
             async with session.get(search) as resp:
@@ -27,3 +31,29 @@ class Search(BaseCog):
                     await ctx.send("No pages found!")
                     return
                 await ctx.send(contents)
+
+    async def get_wolfram_token(self):
+        self.wolframsettings = await self.bot.get_shared_api_tokens("wolfram")
+        self.wolfram_token = self.wolframsettings.get("appid", None)
+        return self.wolfram_token
+
+    @commands.command()
+    async def wolfram(self, ctx, *term: str):
+        """
+        Search Wolfram Alpha
+        """
+        appid = await self.get_wolfram_token()
+        if appid is None:
+            await ctx.send('No token set!')
+            return
+
+        client = wolframalpha.Client(appid)
+        res = client.query(' '.join(term))
+        await ctx.send(next(res.results).text)
+
+
+if __name__ == '__main__':
+    appid = os.environ['WOLFRAM']
+    client = wolframalpha.Client(appid)
+    res = client.query("current weather")
+    print(next(res.results).text)
