454a80779e368a2f25321071bc32265a5d8098ac
Zoe Farmer <zoe@dataleek.io>
2021-07-13 20:13:08 -0600

    move multiple

--- move/move.py
+++ move/move.py
@@ -12,28 +12,29 @@ class Move(BaseCog):
     @commands.command()
     @checks.mod()
     @checks.is_owner()
-    async def move(self, ctx, msg_id: int, new_channel: discord.TextChannel):
-        message = await ctx.message.channel.fetch_message(msg_id)
+    async def move(self, ctx, new_channel: discord.TextChannel, *msg_ids: int):
+        for msg_id in msg_ids:
+            message = await ctx.message.channel.fetch_message(msg_id)
 
-        content = message.content
-        attachments = message.attachments
+            content = message.content
+            attachments = message.attachments
 
-        new_attachments = []
-        if attachments:
-            for a in attachments:
-                x = io.BytesIO()
-                await a.save(x)
+            new_attachments = []
+            if attachments:
+                for a in attachments:
+                    x = io.BytesIO()
+                    await a.save(x)
 
-                x.seek(0)
-                new_attachments.append(
-                    discord.File(x, filename=a.filename, spoiler=a.is_spoiler())
-                )
+                    x.seek(0)
+                    new_attachments.append(
+                        discord.File(x, filename=a.filename, spoiler=a.is_spoiler())
+                    )
 
-        if len(new_attachments) == 0:
-            await new_channel.send(content)
-        elif len(new_attachments) == 1:
-            await new_channel.send(content, file=new_attachments[0])
-        else:
-            await new_channel.send(content, files=new_attachments)
+            if len(new_attachments) == 0:
+                await new_channel.send(content)
+            elif len(new_attachments) == 1:
+                await new_channel.send(content, file=new_attachments[0])
+            else:
+                await new_channel.send(content, files=new_attachments)
 
-        await message.delete()
+            await message.delete()
