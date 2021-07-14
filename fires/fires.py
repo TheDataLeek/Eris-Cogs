7a3563057b017b4a9147eda00100154d2559f7a8
Zoe Farmer <zoe@dataleek.io>
2021-07-13 19:09:32 -0600

    fires timezones

--- fires/fires.py
+++ fires/fires.py
@@ -22,15 +22,7 @@ class Fires(BaseCog):
         """
         Show the firemap
         """
-        date = dt.datetime.now()
+        date = dt.datetime.now() - dt.timedelta(hours=6)
         datestring = date.strftime(self.date_format)
         url = self.url_string.format(datestring)
         await ctx.send(url)
-        # async with aiohttp.ClientSession() as session:
-        #     async with session.get(url) as resp:
-        #         img = await resp.read()
-        #         await ctx.send(
-        #                 file=discord.File(
-        #                     img, filename=f"fires_{datestring}.png"
-        #                     )
-        #                 )
