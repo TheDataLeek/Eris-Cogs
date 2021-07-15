53d461a1f18e6ad2846257dc5e0a3f2d00aac3d7
Zoe Farmer <zoe@dataleek.io>
2021-07-14 21:40:29 -0600

    Hopefully can export from reacts

--- export_emoji/export_emoji.py
+++ export_emoji/export_emoji.py
@@ -3,7 +3,7 @@ import io
 import zipfile
 from zipfile import ZipFile
 
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 # third party
 import discord
@@ -26,29 +26,38 @@ class ExportEmoji(BaseCog):
         Can provide either a list of emoji or the message id of the message with emoji in it or reacted to it.
         String emoji cannot be exported
         """
+        message: discord.Message = ctx.message
         if len(emoji) == 0:
             await ctx.send("No emoji to download!")
             return
 
         buf = io.BytesIO()
+        count = 0
         with zipfile.ZipFile(buf, "w") as zf:
             for e in emoji:
                 if isinstance(e, discord.PartialEmoji) or isinstance(e, discord.Emoji):
                     name, new_buf = await self._export_emoji(e)
                     zf.writestr(name, new_buf.getvalue())
+                    count += 1
                 elif isinstance(e, int):
                     # if int, assume message id
                     message: discord.Message = await ctx.message.channel.fetch_message(e)
-                    reactions = message.reactions
-                    for r in reactions:
-                        react_emoji = r.emoji
-                        if not isinstance(react_emoji, str):
-                            name, new_buf = await self._export_emoji(react_emoji)
-                            zf.writestr(name, new_buf.getvalue())
+                    buf_list = await self._export_from_message(message)
+                    for name, buf in buf_list:
+                        zf.writestr(name, buf.getvalue())
+                        count += 1
+
+            if message.reference:
+                message_id = message.reference.message_id
+                referenced_message = await ctx.message.channel.fetch_message(message_id)
+                buf_list = await self._export_from_message(referenced_message)
+                for name, buf in buf_list:
+                    zf.writestr(name, buf.getvalue())
+                    count += 1
 
         buf.seek(0)
 
-        await ctx.send(file=discord.File(buf, filename="export.zip"))
+        await ctx.send(file=discord.File(buf, filename=f"export_of_{count:0.0f}.zip"))
 
 
     async def _export_emoji(self, emoji: Union[discord.Emoji, discord.PartialEmoji]) -> Tuple[str, io.BytesIO]:
@@ -61,3 +70,14 @@ class ExportEmoji(BaseCog):
         new_buf = io.BytesIO()
         await asset.save(new_buf)
         return name, new_buf
+
+    async def _export_from_message(self, message: discord.Message) -> List[Tuple[str, io.BytesIO]]:
+        reactions = message.reactions
+        results = []
+        for r in reactions:
+            react_emoji = r.emoji
+            if not isinstance(react_emoji, str):
+                name, new_buf = await self._export_emoji(react_emoji)
+                results.append((name, new_buf))
+
+        return results
