5bc132bc96ceb89ee426d6934713562626e50329
Zoe Farmer <zoe@dataleek.io>
2021-12-16 23:18:46 -0700

    december

--- isnt_december_the_best/december.py
+++ isnt_december_the_best/december.py
@@ -13,14 +13,9 @@ class December(BaseCog):
             await user.edit(nick=new_nick)
         except Exception as e:
             print(e)
-            await ctx.send("Sowwy, I'm unable to do this!")
             return
 
-        if 2 > len(new_nick) > 32:
-            await ctx.send("Sowwy, nicks must be between 2 and 32 characters!")
-            return
-
-    @commands.command(aliases=["bn"])
+    @commands.command()
     async def isnt_december_the_best(self, ctx, user: discord.Member, *, new_nick: str = ""):
         for member in ctx.guild.members:
             await self.update_username(ctx, user, "Deer")
