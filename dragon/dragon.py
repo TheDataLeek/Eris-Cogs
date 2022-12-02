58470179c37f63d61b182cc21880aead0f1da409
Zoe Farmer <zoe@dataleek.io>
2022-12-02 13:26:09 -0700

    dragon

--- dragon/dragon.py
+++ dragon/dragon.py
@@ -234,27 +234,26 @@ class Dragon(BaseCog, ErisEventMixin):
 
         ctx = await self.bot.get_context(message)
 
-        async with self.lock_config.channel(message.channel).get_lock():
-            allowed: bool = await self.allowed(ctx, message)
-            if not allowed:
-                return
+        allowed: bool = await self.allowed(ctx, message)
+        if not allowed:
+            return
 
-            dragon = random.choice(dragonart)
-            chunk = 1000
-            if len(dragon) > chunk:
-                n_splits = len(dragon) // chunk
-                # print(f"Dragon too big! Sending in {n_splits} chunks")
-                dragon_lines = dragon.split('\n')
-                chunk_size = (len(dragon_lines) // n_splits) + 1
-                for i in range(n_splits):
-                    cdragon = '\n'.join(dragon_lines[i * chunk_size:(i + 1) * chunk_size])
-                    formatted_dragon = f"```\n{cdragon}\n```"
-                    # print(f"\t({i}) - {len(formatted_dragon)}")
-                    await ctx.send(formatted_dragon)
-            else:
-                await ctx.send(f"```\n{dragon}\n```")
+        dragon = random.choice(dragonart)
+        chunk = 1000
+        if len(dragon) > chunk:
+            n_splits = len(dragon) // chunk
+            # print(f"Dragon too big! Sending in {n_splits} chunks")
+            dragon_lines = dragon.split('\n')
+            chunk_size = (len(dragon_lines) // n_splits) + 1
+            for i in range(n_splits):
+                cdragon = '\n'.join(dragon_lines[i * chunk_size:(i + 1) * chunk_size])
+                formatted_dragon = f"```\n{cdragon}\n```"
+                # print(f"\t({i}) - {len(formatted_dragon)}")
+                await ctx.send(formatted_dragon)
+        else:
+            await ctx.send(f"```\n{dragon}\n```")
 
-            await self.log_last_message(ctx, message)
+        await self.log_last_message(ctx, message)
 
 
 if __name__ == '__main__':
