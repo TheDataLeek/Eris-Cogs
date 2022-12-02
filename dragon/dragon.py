c187338329e5679caf0984a6549754ce0b264ffc
Zoe Farmer <zoe@dataleek.io>
2022-12-02 10:17:29 -0700

    dragon

--- dragon/dragon.py
+++ dragon/dragon.py
@@ -246,8 +246,9 @@ class Dragon(BaseCog, ErisEventMixin):
                 n_splits = len(dragon) // chunk
                 print(f"Dragon too big! Sending in {n_splits} chunks")
                 dragon_lines = dragon.split('\n')
+                chunk_size = (len(dragon_lines) // n_splits) + 1
                 for i in range(n_splits):
-                    cdragon = '\n'.join(dragon_lines[i * chunk:(i + 1) * chunk])
+                    cdragon = '\n'.join(dragon_lines[i * chunk_size:(i + 1) * chunk_size])
                     formatted_dragon = f"```\n{cdragon}\n```"
                     print(f"\t({i}) - {len(formatted_dragon)}")
                     await ctx.send(formatted_dragon)
