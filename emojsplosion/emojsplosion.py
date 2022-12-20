163da9db63a542a3b1e4c3b88ea682e977c4f65b
Zoe Farmer <zoe@dataleek.io>
2022-12-20 12:55:18 -0700

    dragon

--- emojsplosion/emojsplosion.py
+++ emojsplosion/emojsplosion.py
@@ -20,27 +20,28 @@ class EmojSplosion(BaseCog):
 
         self.bot = bot_instance
 
-        self.emojis = {}
-
     async def get_emojis(self, must_contain: str=''):
-        self.emojis = [e for e in self.bot.emojis if must_contain in e.name]
+        return [e for e in self.bot.emojis if must_contain in e.name]
 
-    @commands.command(aliases=['emojsploj'])
+    @commands.command(aliases=['emojsploj', 'exploji'])
     async def emojsplosion(
         self, ctx: commands.Context, must_contain: str=''
     ):
         message: discord.Message = ctx.message
-        await self.get_emojis(must_contain=must_contain)
 
         if not message.reference:
             await ctx.send("Need to reply to a message!")
             return
 
+        emojis = await self.get_emojis(must_contain=must_contain)
+
         channel: discord.Channel = ctx.channel
         referenced: discord.MessageReference = message.reference
         referenced_message: discord.Message = await channel.fetch_message(referenced.message_id)
 
-        emojis = random.sample(self.emojis, 20)
+        if len(emojis) > 20:
+            emojis = random.sample(self.emojis, 20)
+
         for e in emojis:
             try:
                 await referenced_message.add_reaction(e)
