5c4faa1a9004d08755a2e329eb1e58ef90f79a39
Zoe Farmer <zoe@dataleek.io>
2023-07-18 09:01:02 -0600

    Use OpenAI's ChatGPT to respond to the conversation

--- chat/chat.py
+++ chat/chat.py
@@ -1,15 +1,7 @@
-import io
-import os
-from urllib import parse
-from pprint import pprint as pp
-import json
-from typing import List, Dict
+from typing import List, Dict, Union
 
 from redbot.core import commands
 import discord
-import aiohttp
-from redbot.core.utils.chat_formatting import pagify
-from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 import openai
 
 BaseCog = getattr(commands, "Cog", object)
@@ -28,27 +20,29 @@ class Chat(BaseCog):
 
     @commands.command()
     async def chat(self, ctx: commands.Context, *query: str):
-        channel: discord.TextChannel = ctx.channel
-        message: discord.Message = ctx.message
-        author: discord.Member = message.author
-        # chunk: List[discord.Message] = [message async for message in channel.history(limit=50)]
-        # formatted_chunk = '\n'.join([
-        #     f"@{message.author.display_name}: {message.clean_content}"
-        #     for message in chunk
-        # ])
-        # # todo: dynamically find bot's name
-        # openai_query = (
-        #     "You are the robot user named Snek who is mischievous and naughty. "
-        #     "You have a long history of playing pranks on everyone mentioned in the following chat logs. "
-        #     "Given these logs, please provide an appropriate response as your Snek persona."
-        #     f"\n{formatted_chunk}"
-        # )
         if not query:
             return
-        openai_query = " ".join(query)
+
+        channel: discord.abc.Messageable = ctx.channel
+        message: discord.Message = ctx.message
+        author: discord.Member = message.author
+        if isinstance(channel, discord.TextChannel):
+            openai_query = [{"role": "user", "content": " ".join(query)}]
+        elif isinstance(channel, discord.Thread):
+            history = [message async for message in channel.history(limit=100)]
+            history = [
+                {
+                    "role": 'system' if message.author.bot else 'user',
+                    'content': ' '.join(w for w in message.clean_content.split(' ') if w != '.chat')  # todo: prefix
+                }
+            ]
+            openai_query = history
+        else:
+            return
+
         openai.api_key = await self.get_openai_token()
         chat_completion: Dict = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                             messages=[{"role": "user", "content": openai_query}],
+                                                             messages=openai_query
                                                              temperature=1.5)
         try:
             response = chat_completion['choices'][0]['message']['content']
