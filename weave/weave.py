19315f98877bf07f63a7e1e3887373b04a267ee5
Zoe Farmer <zoe@dataleek.io>
2024-01-15 22:31:37 -0700

    chatgpt

--- weave/weave.py
+++ weave/weave.py
@@ -6,6 +6,16 @@ BaseCog = getattr(commands, "Cog", object)
 class Weave(BaseCog):
     def __init__(self, bot):
         self.bot = bot
+        self.all_emoji = None
+
+    async def find_emoji(self):
+        if self.all_emoji is None:
+            guilds = [guild async for guild in self.bot.fetch_guilds(limit=150)]
+            self.all_emoji = dict()
+            for guild in guilds:
+                actual_guild = await self.bot.fetch_guild(guild.id)
+                for e in actual_guild.emojis:
+                    self.all_emoji[e.id] = e
 
     @commands.command()
     async def weave(self, ctx, e1, e2, width: int = 5, length: int = 3):
@@ -13,17 +23,12 @@ class Weave(BaseCog):
         Weaves provided emojis in a grid of specified dimensions. Will error if too large.
         """
         # <a:name:id>
-        guilds = [guild async for guild in self.bot.fetch_guilds(limit=150)]
-        all_emoji = dict()
-        for guild in guilds:
-            actual_guild = await self.bot.fetch_guild(guild.id)
-            for e in actual_guild.emojis:
-                all_emoji[e.id] = e
+        await self.find_emoji()
 
-        e1 = await self.check_emoji(ctx, e1, all_emoji)
+        e1 = await self.check_emoji(ctx, e1)
         if e1 is None:
             return
-        e2 = await self.check_emoji(ctx, e2, all_emoji)
+        e2 = await self.check_emoji(ctx, e2)
         if e2 is None:
             return
 
@@ -42,16 +47,16 @@ class Weave(BaseCog):
         try:
             await ctx.send(msg)
             await ctx.message.delete()
-        except:
-            await ctx.send("Message too long!")
+        except Exception as e:
+            await ctx.send(f"Message too long! {e}")
 
-    async def check_emoji(self, ctx, emoji, all_emoji):
+    async def check_emoji(self, ctx, emoji):
         if ":" in emoji:
             emoji_id = int(emoji[1:-1].split(":")[-1])
-            if emoji_id not in all_emoji:
+            if emoji_id not in self.all_emoji:
                 await ctx.send("Emoji not from any server I'm in!")
                 return None
             else:
-                return all_emoji[emoji_id]
+                return self.all_emoji[emoji_id]
         else:
             return emoji
