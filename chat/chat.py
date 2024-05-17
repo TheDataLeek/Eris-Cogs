276cf21c07a39b6d13d39bce3212b34c6613a633
Zoe Farmer <zoe@dataleek.io>
2024-05-17 13:59:34 -0600

    bugfix

--- chat/chat.py
+++ chat/chat.py
@@ -32,6 +32,8 @@ class Chat(BaseCog):
             guild_name = guild.name
             final_dict[guild_name] = (await whois_config.guild(guild).whois_dict()) or dict()
 
+        print(self.whois_dictionary)
+
         self.whois_dictionary = final_dict
 
     async def contextual_chat_handler(self, message: discord.Message):
