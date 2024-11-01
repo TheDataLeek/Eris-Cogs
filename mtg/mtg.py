f9ca276c9b5dd4f11a3632a1823ced6907be6ac1
Zoe Farmer <zoe@dataleek.io>
2024-11-01 11:30:06 -0600

    MTG card embeds

--- mtg/mtg.py
+++ mtg/mtg.py
@@ -18,6 +18,7 @@ class MTG(BaseCog):
         self.bot: commands.Cog = bot
 
         self.bot.add_listener(self.pull_card_references, "on_message")
+        self.bot.add_listener(self.pull_card_embed, "on_message")
 
     async def pull_card_references(self, message: discord.Message):
         ctx: commands.Context = await self.bot.get_context(message)
@@ -28,24 +29,32 @@ class MTG(BaseCog):
 
         cards = []
         async with aiohttp.ClientSession(headers={"User-Agent": "ErisMTGDiscordBot/1.0", "Accept": "*/*"}) as session:
-            url = "https://api.scryfall.com/cards/named"
             for match in matches:
-                for i in range(2):
-                    fetch_url = f"{url}?format=image&version=png&fuzzy={match}"
-                    if i == 1:
-                        fetch_url = f"{fetch_url}&face=back"
-                    async with session.get(fetch_url) as resp:
-                        if resp.status != 200:
-                            continue
-                        cardbuf = io.BytesIO()
-                        cardbuf.write(await resp.read())
-                        cardbuf.seek(0)
-                        cards.append(discord.File(cardbuf, filename=f"{match}.png"))
-
-                time.sleep(0.1)
+                cards += await query_scryfall(session, match)
 
         channel: discord.TextChannel = ctx.channel
         await channel.send(files=cards)
+
+    async def pull_card_embed(self, message: discord.Message):
+        ctx: commands.Context = await self.bot.get_context(message)
+        text = message.clean_content.lower()
+        matches = re.findall(r"\{\{(.+?)\}\}", text, re.IGNORECASE)
+        if not matches:
+            return
+
+        cards = []
+        async with aiohttp.ClientSession(headers={"User-Agent": "ErisMTGDiscordBot/1.0", "Accept": "*/*"}) as session:
+            for match in matches:
+                cards += await query_scryfall(session, match, datatype="json")
+
+        channel: discord.TextChannel = ctx.channel
+
+        for card in cards:
+            card_embed = discord.Embed(
+                title=card["name"], type="rich", description=card["oracle_text"], url=card["scryfall_uri"]
+            )
+            card_embed.set_image(url=card["image_uris"]["png"])
+            await channel.send(embed=card_embed)
 
     @commands.command()
     async def send_targets(self, ctx: commands.context, *users: discord.Member):
@@ -66,3 +75,31 @@ class MTG(BaseCog):
             base_name = target.name
 
             await channel.send(f"Your target is {name} (@{base_name})")
+
+
+async def query_scryfall(
+    session: aiohttp.ClientSession, card_name: str, datatype="image"
+) -> list[io.BytesIO] | list[dict]:
+    data = []
+    url = "https://api.scryfall.com/cards/named"
+    for i in range(2):
+        if datatype == "image":
+            fetch_url = f"{url}?format=image&version=png&fuzzy={card_name}"
+            if i == 1:
+                fetch_url = f"{fetch_url}&face=back"
+        else:
+            fetch_url = f"{url}?format=json&fuzzy={card_name}"
+            if i == 1:
+                break
+        async with session.get(fetch_url) as resp:
+            if resp.status != 200:
+                continue
+            if datatype == "image":
+                cardbuf = io.BytesIO()
+                cardbuf.write(await resp.read())
+                cardbuf.seek(0)
+                data.append(discord.File(cardbuf, filename=f"{match}.png"))
+            else:
+                data.append(await resp.json())
+        time.sleep(0.1)
+    return data
