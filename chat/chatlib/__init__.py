653ed7343da3b28350328cb9f1fd558e4a6c57da
Zoe Farmer <zoe@dataleek.io>
2025-04-30 22:31:07 -0600

    imports

---
+++ chat/chatlib/__init__.py
@@ -0,0 +1,19 @@
+from .commands import ChatCommands
+# from .images import ImageCommands
+# from .meta import MetaCommands
+# from .pathfinder import PathfinderCommands
+# from .tarot import TarotCommands
+
+
+class Chat(
+    ChatCommands,
+    # ImageCommands,
+    # MetaCommands,
+    # PathfinderCommands,
+    # TarotCommands,
+):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+__all__ = ['Chat']
--- chat/chatlib/commands/__init__.py
+++ chat/chatlib/commands/__init__.py
@@ -1,19 +1,5 @@
 from .chat import ChatCommands
-from .images import ImageCommands
-from .meta import MetaCommands
-from .pathfinder import PathfinderCommands
-from .tarot import TarotCommands
-
-
-class Chat(
-    ChatCommands,
-    # ImageCommands,
-    # MetaCommands,
-    # PathfinderCommands,
-    # TarotCommands,
-):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-__all__ = ['Chat']
+# from .images import ImageCommands
+# from .meta import MetaCommands
+# from .pathfinder import PathfinderCommands
+# from .tarot import TarotCommands
